cdf8e7aa1f1c84a2c7eee557c402f0b3c29d7403
rv <rv.delannoy@gmail.com>
2012-04-02 14:03:23 +0200

    a tester

--- Vrac/Tools/Randomizer.cs
+++ Vrac/Tools/Randomizer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Vrac.Tools
 {
@@ -6,10 +7,54 @@ namespace Vrac.Tools
     {
         #region -> Attributs statiques
 
+        readonly static object _locker = new object();
+        private static bool locked;
         private static Random S_random;
 
         #endregion -> Attributs statiques
 
+
+        static T underLock<T>(Func<T> func)
+        {
+
+            lock (S_random)
+            {
+                while (locked) Monitor.Wait(_locker);
+                locked = true;
+                T r = func();
+                locked = false;
+                Monitor.Pulse(_locker);
+                return r;
+            }
+        }
+
+        static T underLock<P1, T>(Func<P1, T> func, P1 param)
+        {
+
+            lock (S_random)
+            {
+                while (locked) Monitor.Wait(_locker);
+                locked = true;
+                T r = func(param);
+                locked = false;
+                Monitor.Pulse(_locker);
+                return r;
+            }
+        }
+
+        static T underLock<P1, P2, T>(Func<P1, P2, T> func, P1 param1, P2 param2)
+        {
+
+            lock (S_random)
+            {
+                while (locked) Monitor.Wait(_locker);
+                locked = true;
+                T r = func(param1,param2);
+                locked = false;
+                Monitor.Pulse(_locker);
+                return r;
+            }
+        }
 
         #region -> Publiques
 
@@ -19,8 +64,7 @@ namespace Vrac.Tools
         /// <returns>Un nombre aléatoire non négatif.</returns>
         public static int Next()
         {
-            lock(S_random)
-                return S_random.Next();
+            return underLock(S_random.Next);
         }
 
         /// <summary>
@@ -30,8 +74,8 @@ namespace Vrac.Tools
         /// <returns>Un nombre aléatoire non négatif, inférieur au nombre maximal spécifié.</returns>
         public static int Next(int p_maxValue)
         {
-            lock (S_random)
-            return S_random.Next(p_maxValue);
+
+            return underLock(S_random.Next, p_maxValue);
         }
 
         /// <summary>
@@ -42,8 +86,8 @@ namespace Vrac.Tools
         /// <returns>Un nombre aléatoire figurant dans la plage spécifiée.</returns>
         public static int Next(int p_minValue, int p_maxValue)
         {
-            lock (S_random)
-            return S_random.Next(p_minValue, p_maxValue);
+
+            return underLock(S_random.Next, p_minValue, p_maxValue);
         }
 
         /// <summary>
@@ -52,8 +96,8 @@ namespace Vrac.Tools
         /// <returns>Un nombre aléatoire compris entre 0,0 et 1,0.</returns>
         public static double NextDouble()
         {
-            lock (S_random)
-            return S_random.NextDouble();
+            
+            return underLock(S_random.NextDouble);
         }
 
         #endregion -> Publiques
